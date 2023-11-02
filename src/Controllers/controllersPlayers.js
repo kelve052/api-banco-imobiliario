168ee019b0ee4ff72dc12e85ce787024073732d7
kelve Oliveira <102033649+kelve052@users.noreply.github.com>
2023-11-02 11:42:02 -0300

    refactor(controllers) alter if of error remove res.sen e add throw new Error

--- src/Controllers/controllersPlayers.js
+++ src/Controllers/controllersPlayers.js
@@ -2,8 +2,12 @@ import ServicesPlayers from "../Services/servicesPlayer.js"
 const services = new ServicesPlayers()
 
 const playersGet = async (req, res)=>{
-  const get = await services.servicesGetPlayers()
-  res.status(200).json({Players: get})
+  try {
+    const get = await services.servicesGetPlayers()
+    res.status(200).json({Players: get})
+  } catch (error) {
+    res.status(400).json({Error: error.message})
+  }
 }
 //-----------------------------------------------------------------------------------------------------
 const playersPost = async (req, res)=>{
@@ -11,21 +15,18 @@ const playersPost = async (req, res)=>{
    const {name, tean, password} = req.body
    const body = {name, tean, password}
    if(!name || !tean || !password){
-     res.status(400).json({Error: "body incorret, name, tean and password required!"})
-     return;// return vazio é nessesario para não dar erro de ao tentar executar o proximo res.json
+     throw new Error("body incorret, name, tean and password required!")
    }
    if(name.startsWith("Banco") || name.startsWith("banco")){
     throw new Error("Name cannot startWhith Banco")
    }
   if(password.length < 8){
-    res.status(400).json({Error:"The password required minimum 8 caracters!"})
-    return;
+    throw new Error("The password required minimum 8 caracters!")
   }
   const regex = /^(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[0-9]).{8,}$/
   const passwordregex = regex.test(password)
   if(!passwordregex){
-    res.status(400).json({Error: "Mandatory password symbol, a number and capital latter!"})
-    return;
+    throw  new Error("Mandatory password symbol, a number and capital latter!")
   }
   const newPlayer = await services.servicesPostPlayers(body)
   res.status(200).json(newPlayer)
@@ -42,25 +43,21 @@ const playerUpdate = async (req, res)=>{
     const {name, tean, balancer, password} = req.body
     const body = {name, tean, balancer, password}
     if(!name || !tean || !balancer || !password){
-      res.status(400).json({Error: "imcomplete body, required name, tean, balancer and password"})
-      return;
+      throw new Error("imcomplete body, required name, tean, balancer and password")
     }
     if(name.startsWith("Banco") || name.startsWith("banco")){
       throw new Error("Name cannot startWhith Banco")
     }
     if(password.length < 8){
-      res.status(400).json({Error:"The password required minimum 8 caracters!"})
-      return;
+      throw new Error("The password required minimum 8 caracters!")
     }
     const regex = /^(?=.*[!@#$%^&*])(?=.*[A-Z])(?=.*[0-9]).{8,}$/
     const passwordregex = regex.test(password)
     if(!passwordregex){
-      res.status(400).json({Error: "Mandatory password symbol, a number and capital latter!"})
-      return;
+      throw new Error("Mandatory password symbol, a number and capital latter!")
     }
     if(tean != "Yellow" && tean != "Blue" && tean != "White" && tean != "Black" && tean != "Red" && tean != "Green"){
-      res.status(400).json({Error: "value of tean invalid, required: Yellow or Blue or White or Black or Red or Green"})
-      return;
+      throw new Error("value of tean invalid, required: Yellow or Blue or White or Black or Red or Green")
     }
       const newPlayer = await services.servicesPutPlayer(id, body)
       res.status(200).json({Player: newPlayer})
