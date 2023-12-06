83d59f0dd6827b97ce311137eca3e1db8c8f9792
kelve Oliveira <102033649+kelve052@users.noreply.github.com>
2023-12-06 17:16:12 -0300

    refactor (controllerRegister) update in validation fields and delete const postRegisterBanco

--- src/Controllers/controllerRegister.js
+++ src/Controllers/controllerRegister.js
@@ -4,39 +4,16 @@ const servicesRegister = new ServicesRegister()
 
 const postRegister = async (req, res)=>{
   try {
-    const id = req.params.idPlayer
-    const {status, player, balancerValue} = req.body
-    if(!status || !player || !balancerValue){
-      throw new Error("Body required: status (sent), player and balancerValue!")
+    const {playerWhoSent, playerWhoReceived, balanceValue} = req.body
+    if(!playerWhoSent || !playerWhoReceived || !balanceValue){
+      throw new Error("Body required: playerWhoSent , playerWhoReceived and balancerValue!")
     }
-    if(status != "sent"){
-      throw new Error("property estatus required sent!")
-    }
-    const body = {status, player, balancerValue}
-    const newRegister = await servicesRegister.ServicesPostRegister(id, body)
+    const body = {playerWhoSent, playerWhoSent, playerWhoSent}
+    const newRegister = await servicesRegister.ServicesPostRegister(1, body)
     res.status(200).json({Msg: "Operation Sucefull", RegsitePlayers: newRegister})
   } catch (error) {
     res.status(400).json({Error: error.message})
   }
 }
-//-----------------------------------------------------------------------------------------------------
 
-const postRegisterBanco = async (req, res)=>{
-  try {
-    const id = req.params.idBanco
-    const {status, player, balancerValue} = req.body
-    if(!status || !player || !balancerValue){
-      throw new Error("Body required: status (sent, or received), player and balancerValue!")
-    }
-    if(status != "sent" && status != "received"){
-      throw new Error("property estatus required sent or received!")
-    }
-    const body = {status, player, balancerValue}
-    const newRegisterBanco = await servicesRegister.ServicesPostRegisterBanco(id, body)
-    res.status(200).json({Msg: "Operation Sucefull", RegisterBanco: newRegisterBanco})
-  } catch (error) {
-    res.status(400).json({Error: error.message})
-  }
-}
-
-export {postRegister, postRegisterBanco}
+export {postRegister}
